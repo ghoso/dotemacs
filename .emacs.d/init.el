5dc2db420514bb2e64385932279c2b4616ff8c55
Go Hosohara <hosohara@jpcert.or.jp>
2013-10-25 16:08:21 +0900

    Add helm-migemo configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -112,6 +112,14 @@
 ;; 定番パッケージ設定
 ;;
 ;; helm
+(require 'migemo)
+;; for cmigemo
+(setq migemo-command "cmigemo"
+      migemo-options '("-q" "--emacs" "-i" "\g")
+      migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict"
+      migemo-user-dictionary nil
+      migemo-regex-dictionary nil
+      migemo-coding-system 'utf-8)
 (require 'helm-config)
 (require 'helm-migemo)
 ;; Scratchバッファ保存
